ce47c80aa7287cf024112995e5a5413f392c1628
zhoumaomao <zhoumaomao@wecash.net>
2018-06-05 00:40:05 +0800

    添加文件

--- kubaby/pages/save/milk/index.js
+++ kubaby/pages/save/milk/index.js
@@ -1,82 +1,74 @@
-// pages/save/index.js
+var util = require('../../utils/date.js');  
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-   // array: ["吃奶粉", "吃母乳", "睡觉", "尿尿", "粑粑", "洗头洗澡", "记录体重"]
-    array: ["吃奶粉", "吃母乳", "睡觉", "尿尿", "粑粑", "洗头洗澡", "记录体重"],
-    index:0
+    date:null,
+    time:null
   },
-
-  bindPickerChange: function (e) {
-    console.log(e)
-    this.setData({
-      index: e.detail.value
-    })  
-  },
-
-  saveButtonTip:function(e){
-    var id = e.currentTarget.dataset.id
-    wx.navigateTo({
-      url: 'milk/index?id='+id
-    })
-  },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var now = new Date()
+    var date = util.getDate(now)
+    var time = util.getTime(now)
+    this.setData({
+      date:date,
+      time:time
+    }) 
   },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+    
   }
 })
---
+++ kubaby/pages/utils/date.js
@@ -0,0 +1,49 @@
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+function getDate(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/')
+}
+
+
+function getTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+ return [hour, minute, second].map(formatNumber).join(':')
+}
+
+
+
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+module.exports = {
+  formatTime: formatTime,
+  getDate:getDate,
+  getTime:getTime
+} 
